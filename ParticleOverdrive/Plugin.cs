5dd4422f3023b10caa261d3dbda14f2d8a35a5b5
Shadnix <shadnix@arcor.de>
2019-08-28 23:02:51 +0200

    Updated version information for Beat Saber 1.3.0

--- ParticleOverdrive/Plugin.cs
+++ ParticleOverdrive/Plugin.cs
@@ -15,7 +15,7 @@ namespace ParticleOverdrive
     public class Plugin : IBeatSaberPlugin
     {
         public string Name => "Particle Overdive";
-        public string Version => "0.5.1";
+        public string Version => "0.6.0";
 
         private static readonly string[] env = { "Init", "MenuCore", "GameCore", "Credits" };
 
