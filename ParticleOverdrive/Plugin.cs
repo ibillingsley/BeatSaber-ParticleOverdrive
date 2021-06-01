3950f8f11a3fd65be065384dc4703a8686c24413
Shadnix <shadnix@arcor.de>
2021-06-01 02:49:56 +0200

    Version bump 1.11.1

--- ParticleOverdrive/Plugin.cs
+++ ParticleOverdrive/Plugin.cs
@@ -16,7 +16,7 @@ namespace ParticleOverdrive
     public class Plugin
     {
         public string Name => "Particle Overdrive";
-        public string Version => "1.11.0";
+        public string Version => "1.11.1";
         IPA.Logging.Logger log;
 
         private static readonly string[] env = { "Init", "MainMenu", "GameCore", "Credits" };
