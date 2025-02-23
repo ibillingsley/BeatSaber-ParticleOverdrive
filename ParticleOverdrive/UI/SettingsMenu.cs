d276d598eabd19bd8dbcdde376d8d0dcbef8d0ba
Isaiah Billingsley <iza.billingsley@gmail.com>
2025-02-23 17:21:07 -0500

    Allow particle multiplier values between 0 and 1

--- ParticleOverdrive/UI/SettingsMenu.cs
+++ ParticleOverdrive/UI/SettingsMenu.cs
@@ -158,6 +158,15 @@ public class SettingsMenu
     private static readonly List<object> ParticleMultiplierChoicesList =
     [
         0f,
+        0.1f,
+        0.2f,
+        0.3f,
+        0.4f,
+        0.5f,
+        0.6f,
+        0.7f,
+        0.8f,
+        0.9f,
         1f,
         1.1f,
         1.2f,
